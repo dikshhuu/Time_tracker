f832b254fb5aa6e2faed82405a620d1e0e276b0e
Dikshya Khanal <khanaldikshu21@gmail.com>
2026-01-11 20:12:37 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+class TimeConverter
+{
+    // List stores all converted results
+    static List<int> convertedMinutes = new List<int>();
+
+    public static void Main(string[] args)
+    {
+        // Program runs unless the user chooses to stop
+        while (true)
+        {
+            // Reads the input from the user
+            int hours = ReadNumber("Enter hours: ");
+            int minutes = ReadNumber("Enter minutes: ");
+
+            // To check whether the input is valid or not
+            if (minutes < 0 || minutes > 59 || hours < 0)
+            {
+                Console.WriteLine("Invalid input!");
+            }
+            else
+            {
+                // Converts and saves the result
+                int totalMinutes = ConvertToMinutes(hours, minutes);
+                convertedMinutes.Add(totalMinutes);
+                Console.WriteLine("Total time in minutes: " + totalMinutes);
+            }
+
+            // Asks the user to continue or stop 
+            Console.WriteLine("Do you want to continue? (yes/no): ");
+            string answer = Console.ReadLine()??"";
+
+            if (answer != "yes")
+            {
+                break;
+            }
+        }
+        PrintList(); 
+    }
+    // Reads a number from the user
+    public static int ReadNumber(string text)
+    {
+        Console.WriteLine(text);
+        return Convert.ToInt32(Console.ReadLine());
+    }
+    // Converts hours and minutes into total minutes
+    public static int ConvertToMinutes(int hours, int minutes)
+    {
+        return hours * 60 + minutes;
+    }
+    // Prints the list of converted times
+    public static void PrintList()
+    {
+        Console.WriteLine("Converted times:");
+        foreach (int number in convertedMinutes)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
